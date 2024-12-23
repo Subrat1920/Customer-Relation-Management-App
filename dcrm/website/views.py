ccf08a18dca67d10571a7d6bac27b27a89f06e97
Subuking001 <3subratmishra1sep@gmail.com>
2024-12-23 22:42:32 +0530

    new

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    ## checking to see if loggin in
+    if request.method == 'POST':
+        user_name = request.POST['first_name']
+        user_password = request.POST['password']
+
+        ## authenticating
+        user = authenticate(request, username = user_name, password = user_password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged-in")
+            return redirect('home', {})
+        else:
+            messages.success(request, 'Error in loggin, Please try again....')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
